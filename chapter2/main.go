6be13e094af878f2280c090e20e76ffadbec75e6
Juan David Merchan Torres <juan1014166@gmail.com>
2023-06-11 13:28:48 -0500

    feat(chapter2): Add defer, recover & panic in action

--- chapter2/main.go
+++ chapter2/main.go
@@ -6,8 +6,23 @@ import (
 	"github.com/NikolaM-Dev/go-web-book/chapter2/strcon"
 )
 
+func doPanic() {
+	defer func() {
+		if e := recover(); e != nil {
+			fmt.Println("Recover with: ", e)
+		}
+	}()
+
+	panic("Just panicking for the sake of demo")
+	fmt.Println("This will never be called")
+}
+
 func main() {
 	s := strcon.SwapCase("Gohper")
 
 	fmt.Println("Converted string is :", s)
+
+	fmt.Println("Starting to panic")
+	doPanic()
+	fmt.Println("Program regains control after panic recover")
 }
