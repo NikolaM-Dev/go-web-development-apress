56ef59c160a9fb10b59fb62e77d723a6592dcce1
Juan David Merchan Torres <juan1014166@gmail.com>
2023-06-11 14:21:19 -0500

    feat(root): Add composition

--- chapter3/main.go
+++ chapter3/main.go
@@ -20,4 +20,39 @@ func main() {
 
 	nikola.ChangeLocation("New York")
 	nikola.PrintDetails()
+
+	alex := &person.Admin{
+		Person: person.Person{
+			Dob:       time.Date(1970, time.January, 10, 0, 0, 0, 0, time.UTC),
+			Email:     "alex@gmail.com",
+			FirstName: "Alex",
+			LastName:  "John",
+			Location:  "New York",
+		},
+		Roles: []string{
+			"Manage Tasks",
+			"Manage Team",
+		},
+	}
+
+	shiju := &person.Member{
+		Person: person.Person{
+			Dob:       time.Date(1979, time.February, 17, 0, 0, 0, 0, time.UTC),
+			Email:     "shiju@gmail.com",
+			FirstName: "Shiju",
+			LastName:  "Varghese",
+			Location:  "Kochi",
+		},
+		Skills: []string{
+			"Docker",
+			"Go",
+			"Kubernetes",
+		},
+	}
+
+	alex.PrintName()
+	alex.PrintDetails()
+
+	shiju.PrintName()
+	shiju.PrintDetails()
 }
