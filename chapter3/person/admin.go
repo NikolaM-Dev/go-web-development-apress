c013b340c300ed0cdffd3023b572904d47dd5979
Juan David Merchan Torres <juan1014166@gmail.com>
2023-06-11 14:20:20 -0500

    feat(person): Add a override for PrintDetails method

--- chapter3/person/admin.go
+++ chapter3/person/admin.go
@@ -1,6 +1,17 @@
 package person
 
+import "fmt"
+
 type Admin struct {
 	Person
 	Roles []string
+}
+
+func (a *Admin) PrintDetails() {
+	a.Person.PrintDetails()
+
+	fmt.Println("Admin Roles:")
+	for _, r := range a.Roles {
+		fmt.Println(r)
+	}
 }
